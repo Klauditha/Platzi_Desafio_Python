e5123bc978e7a7f24633107499475757a1bcc78a
Claudia <claudia.villarroel@live.com>
2021-11-02 21:12:29 -0300

    ADD: Applying exception management

--- 02_Intermedio/02_CursoPython/debugging.py
+++ 02_Intermedio/02_CursoPython/debugging.py
@@ -1,16 +1,23 @@
 
 def divisors(num):
-    divisor = []
-    for i in range(1,num+1):
-        if num % i == 0:
-            divisor.append(i)
-    return divisor
-
+    try:
+        if num < 0:
+            raise ValueError("Debe ingresar numeros positivos")
+        divisor = []
+        for i in range(1,num+1):
+            if num % i == 0:
+                divisor.append(i)
+        return divisor
+    except ValueError as ve:
+        print(ve)
+        return 0
 def run():
-    num = int(input("Ingresa un numero: "))
-    print(divisors(num))
-    print("Termino mi programa")
-
+    try:
+        num = int(input("Ingresa un numero: "))
+        print(divisors(num))
+        print("Termino mi programa")
+    except ValueError:
+        print("Debes ingresar un numero")
 
 if __name__ == '__main__':
     run()
